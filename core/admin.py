bdf3e308f6540e11fecaddf4e832a18efabd8cfc
Gedionir Paim <67489951+Gedionir@users.noreply.github.com>
2023-03-15 00:19:04 -0300

    Add files via upload

---
+++ core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from core.models import Evento
+
+# Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('titulo', 'usuario', 'data_evento',)    # cria filtro por titulo, usuario e data de evento.
+
+admin.site.register(Evento, EventoAdmin)
